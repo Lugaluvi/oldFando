9c3de484c5760934c98b11716a6635217f5bf10f
LuizKraisch <luizkraisch@users.noreply.github.com>
2020-10-01 18:10:23 -0300

    finished login form

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fando - Organize seu dia de forma prática.</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="d-flex p-2 bd-highlight">
+        <div class="shadow p-3 mb-5 bg-white div-login">
+        <h2><b>Faça seu login</b></h2>
+        <h6 id="emailHelp" class="form-text text-muted">Para acessar a sua lista de tarefas.</h6>
+        <br>
+        <form>
+            <div class="form-group">
+                <label for="user-login"><b>E-mail</b></label>
+                <input type="email" class="form-control" id="user-login" aria-describedby="emailHelp" placeholder="Insira seu email">
+                <small id="emailHelp" class="form-text text-muted">Seu e-mail está segura conosco ;)</small>
+            </div>
+            <div class="form-group">
+                <label for="user-password"><b>Senha</b></label>
+                <input type="password" class="form-control" id="user-password" placeholder="Insira sua senha">
+            </div>
+            <button type="submit" class="btn btn-success"><b>Continuar</b></button>
+        </form>
+        </div>
+    </div>
+    
+</html>
