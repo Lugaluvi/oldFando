47607fe55bc5ae482a85b4f1d1496e85931ddc43
LuizKraisch <luizkraisch@users.noreply.github.com>
2020-11-27 22:22:44 -0300

    Adição do selecione neutro em importância

--- menus/addTask.php
+++ menus/addTask.php
@@ -42,6 +42,7 @@
             <div class="col">
                 <label for="select"><b>É importante?</b></label>
                 <select class="select" name="is_important" required>
+                    <option value="0">Selecione</option>
                     <option value="1">Sim</option>
                     <option value="0">Não</option>
                 </select>
